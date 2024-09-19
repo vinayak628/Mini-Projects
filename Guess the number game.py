80fa7f5d0b0ffb505b7684b029f7793b3f1bb192
vinayak1844 <vishwakarmavinayak67@gmail.com>
2024-09-19 19:27:21 +0530

    first commit

--- Guess the number game.py
+++ Guess the number game.py
@@ -1,7 +1,7 @@
 # Guess the number game
 
 import random
-number=random.randint(1,5)
+number=random.randint(1,5)              #it generates random numbers within the given range
 print("Guess a number between 1-5...")
 attempt=0
 Q=False
@@ -9,29 +9,30 @@ Q=False
 while True:
     x=(input("Guess the number or Quit(Q):"))
     if(x=="Q"):
-        attempt+=1
+        attempt+=1               #incrementing attempt if a number is guessed
         print("Thankyou!!")
         Q=True
         break
 
-    x=int(x)
+    x=int(x)      #converting into integer
 
     if (x==number):
         print("Hurrrray....!!!You guessed the correct number...!")
-        attempt+=1
+        attempt+=1               #incrementing attempt for the correct guess
         break
     
     if(x<number):
         print("The number you have guessed is smaller...")
-        attempt+=1
+        attempt+=1               #incrementing for a guess
 
     elif(x>number):
         print("The number you have entered is greater...")
-        attempt+=1
+        attempt+=1              ##incrementing for a guess
 
     else:
         print("Invalid Number")
 
-if Q==False:
+if Q==False:       #if player does not choose to exit this loop runs!!
     print("You have completed the game in",attempt,"attempts!!")
-print("---GAME OVER---")
+
+print("---GAME OVER---")               
--- Password generator type 2.py
+++ Password generator type 2.py
@@ -1,10 +1,20 @@
 # RANDOM PASSWORD GENERATOR 
-import random
-import string
+import random               # a python module which generates random choice
+import string               # a python library which contains digits,ascii values and characters
 
-length=int(input("Enter the length of the password:"))
-character=string.ascii_letters+string.digits+string.punctuation
+length=int(input("Enter the length of the password:"))         # input from the user
 
-password="".join([random.choice(character) for i in range(length)])
 
+#using string module to call digits,characters and ascii values
+character=string.ascii_letters+string.digits+string.punctuation        
+
+
+#creating a list and joining it using ".join" 
+
+#random.choice(character)= display a random character from the string module
+
+password="".join([random.choice(character) for i in range(length)])      
+
+
+#printing the password
 print(password)
